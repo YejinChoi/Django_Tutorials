207de62c1d361193af430299c0b90b1f9ec05da4
= <=>
2018-01-17 14:52:43 +0900

    accounts.models.모델 생성/ad 등록

--- askdjango/accounts/models.py
+++ askdjango/accounts/models.py
@@ -1,3 +1,10 @@
+#accounts/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# 한 명의 유저에 대해 한 개의 프로필 = OneToOneField 설정
+class Profile(models.Model):
+    user = models.OneToOneField(User) #FIXME : BAD CASE!!!
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
