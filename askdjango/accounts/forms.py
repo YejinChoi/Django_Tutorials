875fed34849b27bd3587cdc2186e38bac4388385
YejinChoi <cyj95428@naver.com>
2018-01-22 02:53:09 +0900

    회원가입시에 이메일 추가 입력

--- askdjango/accounts/forms.py
+++ askdjango/accounts/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 
+# 회원가입시에 이메일 추가 입력
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email',)
--- askdjango/accounts/views.py
+++ askdjango/accounts/views.py
@@ -2,18 +2,18 @@
 from django.conf import settings
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
 
 # Create your views here.
 # 회원가입 구현
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect(settings.LOGIN_URL) # default : "/accounts/login/"
     else:
-        form = UserCreationForm()
+        form = SignupForm()
 
     return render(request, 'accounts/signup_form.html',{
         'form': form,
